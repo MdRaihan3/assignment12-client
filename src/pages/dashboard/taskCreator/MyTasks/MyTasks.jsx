6968e9a59bbd42d4752f465614558289666252c3
MdRaihan3 <1mdr91012@gmail.com>
2024-06-07 21:13:48 +0600

    updated

--- src/pages/dashboard/taskCreator/MyTasks/MyTasks.jsx
+++ src/pages/dashboard/taskCreator/MyTasks/MyTasks.jsx
@@ -23,7 +23,7 @@ const MyTasks = () => {
     const handleDeleteTask = (task) => {
         Swal.fire({
             title: "Are you sure?",
-            text: "You won't be able to revert this safsadf!",
+            text: "You won't be able to revert this!",
             icon: "warning",
             showCancelButton: true,
             confirmButtonColor: "#3085d6",
